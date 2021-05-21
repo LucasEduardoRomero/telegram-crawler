d5f640fc38874c96eb1f0a61ff057152515c12fe
LucasEduardoRomero <lucasromero.cba@hotmail.com>
2021-05-21 16:25:17 -0400

    Implementando funcao que monta conjunto de abas e melhorando modularizacao

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import json
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.keys import Keys
 
 
 def load_config(config_file):
@@ -15,11 +15,16 @@ class TipScanner():
     def __init__(self, config_file):
         conf = load_config(config_file)        
         
+        self.base_url = "https://web.telegram.org/#/im?p="
+        
+        # Setting user dir of chrome config
         self.user_data_dir = f"user-data-dir={conf['user_data_dir']}"
         self.profile_dir = f"--profile-directory={conf['profile_dir']}"
-
+        # setting channels to be scrapped and last msg will store de Serial ID of every message
         self.channels = conf['channelsId']
-        self.last_msg_id = -1
+        self.storage = {}
+        for channel in conf['channelsId']:
+            self.storage[channel] = -1
 
         options = webdriver.ChromeOptions()
         options.add_argument(self.user_data_dir)
@@ -30,9 +35,23 @@ class TipScanner():
             executable_path=conf['executable_path'],
             chrome_options=options
         )
-        self.driver.get(f"https://web.telegram.org/#/im?p={self.channels[0]}")    
 
-    def monitor(self):            
+    def mount_pages(self):
+        self.driver.get(self.base_url + self.channels[0])
+        time.sleep(5)
+        body = False
+        # TODO -> NOT WORKING. fazer programa espera body ficar pronto ( WebDriver.Wait )
+        for channels in self.channels[1:]:
+            print("tentando abrir paginas")
+            body = self.driver.find_element_by_xpath("//body")
+            body.send_keys(Keys.CONTROL+ "t")
+            body.send_keys(Keys.TAB)
+        if(body):
+            body.send_keys(Keys.TAB)        
+
+    def monitor(self, target_url):
+        # TODO -> search target page
+
         new_msg_xpath = ".//div[contains (@class, 'im_message_outer_wrap') and contains (@class, 'hasselect')]"
         new_message_arr = self.driver.find_elements_by_xpath(new_msg_xpath)         
         if(len(new_message_arr) > 0):      
@@ -50,7 +69,7 @@ class TipScanner():
         else:
             print("\nNenhuma Mensagem Encontrada\n")
 
-    def scan(self):        
+    def test(self):        
         self.driver.get(f"https://web.telegram.org/#/im?p={self.channels[0]}")
         
         time.sleep(5) # Let the user actually see something!
@@ -66,16 +85,21 @@ class TipScanner():
 
         time.sleep(5)
 
-
-
+    def terminate(self):
         self.driver.quit()
 
 if __name__ == "__main__":
-    tips = TipScanner('config.json')    
+    tips = TipScanner('config.json')
+    time.sleep(2)
+    tips.mount_pages()
+    time.sleep(5)
+
     try:
         while True:
-            tips.monitor()
+            for channel in tips.channels:
+                tips.monitor(tips.base_url + channel)
             print("-------------------")
             time.sleep(2)
     except Exception as err:
-        tips.driver.quit()
+        print(f"\nShowing Error:\n{err}")
+        tips.terminate()
